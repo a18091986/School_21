33ef121c3bfd5eabcbff42eaba1b5078784511ca
Andrey <a18091986@gmail.com>
2023-03-09 20:20:15 +0300

    -

--- 2_Base_Study/Decimal/src/bin_digits_test.c
+++ 2_Base_Study/Decimal/src/bin_digits_test.c
@@ -1,17 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// int main() {
-
-//     unsigned int d1 = 0b111;
-//     printf("d1: %d\n", ~d1);
-
-//     return 0;
-// }
-
-// Вывод байта в двоичном виде
-
-typedef unsigned char byte;
+#define RED "\033[1;31m"
+#define RESET   "\033[0m"
 
 char * print_number_in_binary_view(int x) {
     int mask = 0b10000000;
@@ -36,7 +27,22 @@ int get_bit_in_position(int x, int bit_position) {
 int main() {
   int number = 93;
   int bit_position = 2;
-  printf("--------------get_bit_function_check--------------------\n");
-  printf("Number %d, in binary view: %s, bit on %d position is %d\n", number, print_number_in_binary_view(number), bit_position, get_bit_in_position(number, bit_position));
+  printf("%s------------------------------get_bit_function_check-------------------------------------------------%s\n", RED, RESET);
+  printf("Number %d, in binary view: %s, bit on %d position is %d\n\n", number, print_number_in_binary_view(number), bit_position, get_bit_in_position(number, bit_position));
+  printf("%s-----------------------------------------------------------------------------------------------------%s\n", RED, RESET);
+
   return 0;
 }
+
+// #include <stdio.h>
+
+// #define RESET   "\033[0m"
+// #define RED     "\033[1;31m"
+// #define YELLOW  "\033[1;33m"
+// #define WHITE   "\033[1;37m"
+
+// int main() {
+//     printf("%s00%s\n",    YELLOW, RESET);
+//     printf("%s===3%s\n", RED,    RESET);
+//     printf("%s-.%s\n",  WHITE,  RESET);
+// }
