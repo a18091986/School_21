fc5162bba237b118949e7d190ad790d385697fe3
behappyman <behappyman@mail.ru>
2023-06-29 14:50:06 +0300

    task_3

---
+++ 2_Base_Study/Docker/DO5_SimpleDocker-1/src/part3/web_server.c
@@ -0,0 +1,10 @@
+#include "fcgi_stdio.h"
+#include <stdlib.h>
+
+int main (void) {
+    while (FCGI_Accept() >= 0) {
+        printf("Connect-type: text/html\r\nStatus: 200 OK\r\n\r\n"
+        "Hello World!");
+    }
+    return 0;
+}
