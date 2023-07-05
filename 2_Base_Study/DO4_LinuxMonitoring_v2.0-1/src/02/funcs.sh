d91171499ea984f37540292fec2f6c0b42e4f427
from linux for school21 <from linux for school21>
2023-07-05 10:08:10 +0000

    start task_3

--- 2_Base_Study/DO4_LinuxMonitoring_v2.0-1/src/02/funcs.sh
+++ 2_Base_Study/DO4_LinuxMonitoring_v2.0-1/src/02/funcs.sh
@@ -2,18 +2,28 @@
 
 closed_dirs_regexp='\/[s]?bin'
 
+function create {
+    while [[ $DIRECTORIES_COUNT_CREATED != $DIRECTORIES_COUNT ]]
+    do
+        ABS_PATH=$start_path
+        cd $ABS_PATH
+        choose_dir_path
+    done
+}
+
 function choose_dir_path {
+    # echo "$DIRECTORIES_COUNT_CREATED"
     random_num=$(echo $(( $RANDOM % 2 )))
     dirs_in_cur_dir=$(echo $(ls -l -d $ABS_PATH/* 2>/dev/null | wc -l))
     dir_name=""
 
     
     if [[ $dirs_in_cur_dir == 1 || $dirs_in_cur_dir == 0 || $random_num == 0  ]]; then :
-        if [[ $letters_for_dirs_count < 4 ]]; then
-            for (( i = 0; i < 5 - $letters_for_dirs_count; i++ )) do 
+        if [[ $letters_for_dirs_count < 5 ]]; then
+            for (( i = 0; i < 6 - $letters_for_dirs_count; i++ )) do 
                 dir_name+=${LETTERS_FOR_DIRS:0:1}
             done
-            for (( j = 1; j < $n + 2; j++ )) do 
+            for (( j = 1; j < $dirs_in_cur_dir + 2; j++ )) do 
                 dir_name+=${LETTERS_FOR_DIRS:0:$letters_for_dirs_count}
             done
         else
@@ -45,25 +55,14 @@ function choose_dir_path {
         fi        
     fi
 }
-
-
-function create {
-    # for (( n = 0; n < $DIRECTORIES_COUNT; n++ )) do
-    while [[ $DIRECTORIES_COUNT_CREATED != $DIRECTORIES_COUNT ]]
-    do
-        ABS_PATH=$start_path
-        cd $ABS_PATH
-        choose_dir_path
-    done
-}
         
 function create_files {
     FILES_COUNT=$(echo $(( 1 + $RANDOM % 100 )))
     for (( num = 0; num < $FILES_COUNT; num++ ))
     do
         file_name=""
-        if [[ $letters_for_files_count < 4 ]]; then
-            for (( i = 0; i < 5 - $letters_for_files_count; i++ )) do 
+        if [[ $letters_for_files_count < 5 ]]; then
+            for (( i = 0; i < 6 - $letters_for_files_count; i++ )) do 
                 file_name+=${LETTERS_FOR_FILES_NAMES:0:1}
             done
             for (( j = 1; j < $num + 2; j++ )) do 
@@ -77,11 +76,19 @@ function create_files {
         fi
         DATE_PART=$(date +%D | awk -F / '{print $2$1$3}')   
         file_name+="_$DATE_PART.$FILE_EXT"
-        sudo fallocate -l $size"MB" $ABS_PATH/$dir_name/$file_name
+        sudo fallocate -l $size"MB" $ABS_PATH/$dir_name/$file_name 2>/dev/null
         FREE_SPACE_MB=$(df -h / |  head -2 | tail +2 | awk '{printf("%d", $4)}')
         if [[ $FREE_SPACE_MB -le 1 ]]; then 
             echo "Memory limit exceeded"
-            exit 1
+            
+            end_nano=$(date +'%s%N')
+            end_time=$(date +'%Y-%m-%d %H:%M:%S')
+            DIFF=$((( $end_nano - $start_nano ) / 1000000 ))
+            echo "End time: $end_time" > log.txt
+            echo "Script work duration: $DIF" > log.txt
+
+            echo -e "Start time: $start_time\nEnd time: $end_time\nDuration: $DIFF ms"
+            exit
         fi
         echo $ABS_PATH$dir_name/$file_name "|" $(date +'%e.%m.%Y') "|" >> log.txt    
     done
--- 2_Base_Study/DO4_LinuxMonitoring_v2.0-1/src/02/main.sh
+++ 2_Base_Study/DO4_LinuxMonitoring_v2.0-1/src/02/main.sh
@@ -20,8 +20,8 @@ source ./validate.sh $LETTERS_FOR_DIRS \
                      $LETTERS_FOR_FILES \
                      $FILE_SIZE_MB
 
-# rm -r /home/behappyman/out/*
-
+sudo rm -rf /test_dir/*
+rm log.txt
 touch log.txt
 
 LETTERS_FOR_FILES_NAMES=$(echo "$LETTERS_FOR_FILES" | awk '{split($0,a,"."); print a[1]}')
@@ -29,21 +29,19 @@ FILE_EXT=$(echo "$LETTERS_FOR_FILES" | awk '{split($0,a,"."); print a[2]}')
 letters_for_dirs_count=${#LETTERS_FOR_DIRS}
 letters_for_files_count=${#LETTERS_FOR_FILES_NAMES}
 
-# DIRECTORIES_COUNT=$(echo $(( 1 + $RANDOM % 100 )))
-DIRECTORIES_COUNT=3
+DIRECTORIES_COUNT=$(echo $(( 1 + $RANDOM % 100 )))
 DIRECTORIES_COUNT_CREATED=0
 
-# start_nano=$(date +'%s%N')
-# start_time=$(date +'%Y-%m-%d %H:%M:%S')
-# echo "Start time: $start_time" > log.txt
+start_nano=$(date +'%s%N')
+start_time=$(date +'%Y-%m-%d %H:%M:%S')
+echo "Start time: $start_time" > log.txt
 
 create
 
-# end_nano=$(date +'%s%N')
-# end_time=$(date +'%Y-%m-%d %H:%M:%S')
-# echo "End time: $end_time" > log.txt
-# echo "Script work duration: $start_time - $end_time" > log.txt
+end_nano=$(date +'%s%N')
+end_time=$(date +'%Y-%m-%d %H:%M:%S')
+DIFF=$((( $end_nano - $start_nano ) / 1000000 ))
+echo "End time: $end_time" > log.txt
+echo "Script work duration: $DIF" > log.txt
 
-# echo -e "Start time: $start_time \n \
-#          End time: $end_time \n \
-#          Duration: $start_time - $end_time"
+echo -e "Start time: $start_time\nEnd time: $end_time\nDuration: $DIFF ms"
