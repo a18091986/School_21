f9c99ebdbb041078602ab46e21c3883f2c6ae95f
behappyman <a18091986>
2023-10-09 14:00:48 +0300

    day-02

--- 2_Base_Study/SQL/SQL_beginner.Day01-1/src/ex06/day01_ex06.sql
+++ 2_Base_Study/SQL/SQL_beginner.Day01-1/src/ex06/day01_ex06.sql
@@ -8,8 +8,8 @@
 -- Let's return our mind back to exercise #03 and change our SQL statement to return person names instead of person identifiers and change ordering by action_date in ascending mode and then by person_name in descending mode. Please take a look at a data sample below.
 
 
-(SELECT order_date AS action_date, (SELECT NAME FROM person WHERE person.id = person_order.person_id) AS NAME FROM person_order)
+(SELECT order_date AS action_date, (SELECT NAME FROM person WHERE person.id = person_order.person_id) AS person_name FROM person_order)
 INTERSECT
-(SELECT visit_date AS action_date, (SELECT NAME FROM person WHERE person.id = person_visits.person_id) AS NAME FROM person_visits)
-ORDER BY action_date, NAME DESC
+(SELECT visit_date AS action_date, (SELECT NAME FROM person WHERE person.id = person_visits.person_id) AS person_name FROM person_visits)
+ORDER BY action_date, person_name DESC
 
--- 2_Base_Study/SQL/SQL_beginner.Day01-1/src/ex07/day01_ex07.sql
+++ 2_Base_Study/SQL/SQL_beginner.Day01-1/src/ex07/day01_ex07.sql
@@ -7,7 +7,7 @@
 
 -- Please write a SQL statement which returns the date of order from the person_order table and corresponding person name (name and age are formatted as in the data sample below) which made an order from the person table. Add a sort by both columns in ascending mode.
 
-select 
+SELECT 
     order_date, CONCAT(name, ' (age:', age, ')') AS person_information 
 FROM 
     person_order
