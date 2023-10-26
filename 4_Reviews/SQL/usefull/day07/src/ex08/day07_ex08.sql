c980c0492ac64c336323071dc8ddfac5c53ec8ce
Your Name <you@example.com>
2023-10-26 13:51:21 +0300

    day07

--- 4_Reviews/SQL/usefull/day07/src/ex08/day07_ex08.sql
+++ 4_Reviews/SQL/usefull/day07/src/ex08/day07_ex08.sql
@@ -11,4 +11,10 @@
 -- Kazan	DinoPizza	4
 -- ...	...	...
 
-
+SELECT address, pizzeria.name name, COUNT(*) as count_of_orders
+FROM person
+JOIN person_order ON person.id = person_order.person_id
+JOIN menu ON person_order.menu_id = menu.id
+JOIN pizzeria ON pizzeria.id = menu.pizzeria_id
+GROUP BY address, pizzeria.name
+ORDER BY 1, 2;
