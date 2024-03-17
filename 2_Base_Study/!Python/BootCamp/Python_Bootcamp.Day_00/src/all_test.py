389c78b72936df53f0e884d0c2b831abadabd43f
behappyman <behappyman@linux.mint.garaj>
2024-03-18 02:19:36 +0300

    start test

--- 2_Base_Study/!Python/BootCamp/Python_Bootcamp.Day_00/src/all_test.py
+++ 2_Base_Study/!Python/BootCamp/Python_Bootcamp.Day_00/src/all_test.py
@@ -11,8 +11,16 @@
 
 
 import subprocess
-command = r"type for_tests/ex01/data_hashes_10lines.txt | python blocks.py 20"
+command = r"cat for_tests/ex01/data_hashes_10lines.txt | python blocks.py 20"
 
 result = subprocess.run(command, capture_output=True, text=True, shell=True)
-print(result)
+correct = """00000254b208c0f43409d8dc00439896
+0000085a34260d1c84e89865c210ceb4
+0000071f49cffeaea4184be3d507086v
+В поданном на вход файле меньше строк, чем было указано, завершение программы
+"""
+# print(result)
 print(result.stdout)
+print(correct)
+print(result.stdout == correct)
+assert(result.stdout == correct)
